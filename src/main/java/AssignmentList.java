d3d2243d92d7e50da85db3012cdc11e900e4fdc3
jfonsec3 <31457695+jfonsec3@users.noreply.github.com>
2017-10-11 00:29:34 -0700

    Update AssignmentList.java

--- src/main/java/AssignmentList.java
+++ src/main/java/AssignmentList.java
@@ -1,50 +1,73 @@
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.*;
-
 class AssignmentList {
+	/**
+	 * This class will create an object used to store assignment information--name,
+	 * deadline, and points.
+	 * Getting and setting each are available
+	 * @author Jacqueline Fonseca
+	 * @version 10/10/2017
+	 */
+	
 	private String name, date;
-	private int points;
-    
-	List<AssignmentFiles> assignmentFiles = new ArrayList<AssignmentFiles>();
-	public boolean setName(String name)
+	private int points;	
+	
+	/**
+	 * Sets the assignment name
+	 * @param assuignment name
+	 */
+	public void setName(String name)
 	{
 		this.name = name;
-		return true;
 	}
+	
+	/**
+	 * @return assignment name
+	 */
 	public String getName()
 	{
 		return this.name;
 	}
 	
-	public boolean setDate(String date)
+	/**
+	 * Sets the assignment deadline
+	 * @param assignment date
+	 */
+	public void setDate(String date)
 	{
 		this.date = date;
-		return true;
 	}
 	
+	/**
+	 * @return assignment deadline
+	 */
 	public String getDate()
 	{
 		return this.date;
 	}
-	
-	public boolean setPoints(int points)
+	/**
+	 *  Sets the total points of an assignment
+	 * @param assignment points
+	 */
+	public void setPoints(int points)
 	{
 		this.points = points;
-		return true;
 	}
 	
+	/**
+	 * @return assignment points
+	 */
 	public int getPoints()
 	{
 		return this.points;
 	}
 	
+	/**
+	 * Used to format the way assignment information will
+	 * be displayed
+	 */
 	@Override
 	public String toString()
 	{
-		return "Assignment Name: " + getName() + "\t\t" + "Assignment Due Date: " + getDate() 
-			+ "\t\t" + "Total Points: "+ Integer.toString(getPoints());
+		return "<html> Assignment Name: " + getName() + "&#8195;" + "Assignment Due Date: " + getDate()
+	    + "&#8195;" + "Total Points: "+ Integer.toString(getPoints()) + "</html>";
 	}
-
 }
