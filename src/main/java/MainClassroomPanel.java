89bea1d8616b1e1bb78934673f76b02f12d9b061
jfonsec3 <31457695+jfonsec3@users.noreply.github.com>
2017-10-11 00:38:08 -0700

    Update MainClassroomPanel.java

--- src/main/java/MainClassroomPanel.java
+++ src/main/java/MainClassroomPanel.java
@@ -3,45 +3,93 @@ import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.io.*;
+import java.nio.file.*;
+import java.util.List;
+import java.util.stream.Stream;
 import javax.swing.*;
 
+/**
+ * This class is the window for the instructor's classroom.
+ * It will allow the user to:
+ * 	- view current assignments
+ * 	- edit assignments
+ * 	- view questions from the Question Bank
+ * 
+ * @author Jacqueline Fonseca
+ * @version 10/10/17
+ */
 public class MainClassroomPanel extends JFrame implements ActionListener{
 	
 	private static final long serialVersionUID = 1L;
 
+	private static final Path FILES_PATH = Paths.get(AssignmentQuestions.class.getResource("assignment_files").getPath());
 	private static final Dimension PREFERRED_RESOLUTION = new Dimension(1280, 720);
-	private JButton addItem, editQuestions, editAssignments; 
+	private JButton viewQuestions, exitQuestions, exit; 
 	private JPanel main;
+	List<String> lines;
 	Assignments assignment;
 	AssignmentQuestions questions;
-	
 	CardLayout cardLayout;
+	
+	
 	MainClassroomPanel()
 	{
 		this.setPreferredSize(PREFERRED_RESOLUTION);
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(MainClassroomPanel.DISPOSE_ON_CLOSE);
 		setTitle("Instructor's Classroom");
-		
+				
+		 //Creates panels for viewing assignments and questions 
 		assignment = new Assignments();
-		JPanel questions = new AssignmentQuestions();
-		addItem = new JButton("Add an assignment");
-        addItem.addActionListener(this);
-        editQuestions = new JButton("Edit assignment questions");
-        editQuestions.addActionListener(this);
-        editAssignments = new JButton("Save & Exit");
-        editAssignments.addActionListener(this);
-        editAssignments.setVisible(false);
-        JPanel addButtons = new JPanel();
-        addButtons.add(addItem);
-        addButtons.add(editQuestions);
-        addButtons.add(editAssignments);
+		questions = new AssignmentQuestions();
 		
+		//Creates all buttons used for alternating between panels and exiting the program
+		JPanel questionsPanel = new AssignmentQuestions();
+        viewQuestions = new JButton("View all questions");
+        viewQuestions.addActionListener(this);
+        exitQuestions = new JButton("Exit Questions");
+        exitQuestions.addActionListener(this);
+        exitQuestions.setVisible(false);
+        exit = new JButton("Exit Classroom");
+        exit.addActionListener(this);
+        
+        //Buttons added components to panel and placed on top of the window
+        JPanel addButtons = new JPanel();
+        addButtons.add(viewQuestions);
+        addButtons.add(exitQuestions);
+        addButtons.add(exit);
+        add(addButtons, BorderLayout.NORTH);
+        
+        //Card layout used to alternate the panel being shown 
         main = new JPanel(new CardLayout());
 		main.add(assignment, "1");
-		main.add(questions, "2");
+		main.add(questionsPanel, "2");
 		add(main, BorderLayout.CENTER);
-		add(addButtons, BorderLayout.NORTH);
+        
+		//Reads all of the files containing assignments
+        try (Stream<Path> assignmentPath = Files.walk(FILES_PATH)) {
+          assignmentPath.filter(Files::isRegularFile).forEach(path -> {
+        	  
+    				  try {
+    					//stores each line in a file into an ArrayList
+					this.lines = Files.readAllLines(path);
+					
+					//Adds name, date, and points from the ArrayList into a new object 
+		        		AssignmentList al = new AssignmentList();
+		        		al.setName((lines.get(0)).replace("Name: ", ""));
+		        		al.setDate((lines.get(1)).replace("Deadline: ", ""));
+		        		al.setPoints(Integer.parseInt((lines.get(2)).replace("Points: ", "")));
+		        		
+		        		//sends to assignment panel to be displayed
+		        		assignment.loadAssignments(al);						
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+    			});
+    		} catch (IOException e) {
+          e.printStackTrace();
+        }
+        
 		pack();
 		setVisible(true);
 	}
@@ -51,31 +99,56 @@ public class MainClassroomPanel extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		
 		Object source = e.getSource();
-		if (source == addItem)
-			assignment.addItems();
-		if (source == editQuestions)
+
+		//Changes panels to view questions
+		if (source == viewQuestions)
 		{
-			if (assignment.getSelectedIndex() < 0)
-			{
-				JOptionPane.showMessageDialog(this, "Select an assignment.");
-				return;
-			}
+			viewQuestions.setVisible(false);
+			exitQuestions.setVisible(true);
 			cardLayout = (CardLayout)(main.getLayout());
 			cardLayout.show(main, "2");
-			editQuestions.setVisible(false);
-			addItem.setVisible(false);
-			editAssignments.setVisible(true);
 		}
 		
-		if (source == editAssignments)
+		//Changes panels to view current assignments
+		if (source == exitQuestions)
 		{	
 			cardLayout = (CardLayout)(main.getLayout());
 			cardLayout.show(main, "1");
-			editQuestions.setVisible(true);
-			editAssignments.setVisible(false);
+			viewQuestions.setVisible(true);
+			exitQuestions.setVisible(false);
+			return;
+			
 		}
-
+		 //Exits the classroom and saves all of the current assignments
+		if (source == exit)
+		{
+			//Gets the current list of assignments
+			List<AssignmentList> list = (this.assignment).returnAssignments();
+			int index = 0;
+			//Goes through all of the files containing assignments
+			try (Stream<Path> assignmentPath = Files.walk(FILES_PATH)) {
+			    assignmentPath.filter(Files::isRegularFile).forEach(path -> {
+			    		try {
+			    			//stores assignment into a string
+			    			String assignmentInfo = "Name: " + (list.get(index)).getName() + "\nDeadline: " + (list.get(index)).getDate() +
+			    					"\nPoints: " + (list.get(index)).getPoints();
+			    			FileWriter writer = new FileWriter(path.toString());
+			    			
+			    			//writes the assignment into the file
+			    			writer.write("");
+			    			writer.append(assignmentInfo);
+			    			writer.flush();
+			    			writer.close();
+			    			list.remove(index);						
+						} catch (IOException ex) {
+								ex.printStackTrace();
+						}
+			    			});
+			    		} catch (IOException ex) {
+			          ex.printStackTrace();
+			    		}
+	
+			this.dispose();
+		}
 	}
-	
-	
 }
