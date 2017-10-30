16eeab9747d0bb68b921ce103a711a1214dfdc85
mlill <mtlillie@asu.edu>
2017-10-29 18:17:39 -0700

    Added a Stopwatch class to more easily track the duration of each lesson that we give the student. Also made the 'calcualteStatus' method, dunno if it's any good.

--- src/main/java/ControlCenter.java
+++ src/main/java/ControlCenter.java
@@ -1,10 +1,12 @@
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * ControlCenter class that uses the Singleton design pattern to be able to track the number of correct and incorrect answers
  * of each lesson. Also this will track the time spent on each question of each lesson and calculate the current student's status.
  *
  * @author Matt Lillie
- * @version 10/23/2017
+ * @version 10/29/2017
  */
 public class ControlCenter {
 
@@ -17,12 +19,18 @@ public class ControlCenter {
     //The arrays associated with the lessons.
     private final int[] correctAnswers;
     private final int[] incorrectAnswers;
-    private final long[] timeSpent;
+    private final Stopwatch[] stopwatches;
 
+    /**
+     * Constructs new ControlCenter and initializes variables
+     */
     private ControlCenter() {
         correctAnswers = new int[NUMBER_OF_LESSONS];
         incorrectAnswers = new int[NUMBER_OF_LESSONS];
-        timeSpent = new long[NUMBER_OF_LESSONS];
+        stopwatches = new Stopwatch[NUMBER_OF_LESSONS];
+        for(int i = 0; i < NUMBER_OF_LESSONS; i ++) {
+            stopwatches[i] = new Stopwatch(TimeUnit.SECONDS);
+        }
     }
 
     /**
@@ -53,20 +61,49 @@ public class ControlCenter {
     }
 
     /**
-     * Gets the time spent on each question array.
-     * @return The time spent array.
+     * Gets the stopwatches.
+     * @return The stopwatches array.
      */
-    public long[] getTimeSpent() {
-        return timeSpent;
+    public Stopwatch[] getStopwatches() {
+        return stopwatches;
     }
 
     /**
      * Calculates the status of the student by going through each array and deciding whether or not the student is doing well or
-     * not. TODO
+     * not.
      * @return A StudentStatus that will be either: worried, happy, thinking, or sorry.
      */
     public StudentStatus calculateStatus() {
-        return null;
+        int totalCorrect = 0, totalWrong = 0;
+        long totalTime = 0;
+
+        for(int correct : correctAnswers) {
+            totalCorrect += correct;
+        }
+
+        for(int wrong : incorrectAnswers) {
+            totalWrong += wrong;
+        }
+
+        for(Stopwatch stopwatch : stopwatches) {
+            totalTime += stopwatch.elapsedTime(); // in seconds
+        }
+
+        if(totalWrong > totalCorrect) { // More wrong than correct
+            if(totalTime > 300) { // 5 minutes in seconds
+                return StudentStatus.WORRIED;
+            } else {
+                return StudentStatus.SORRY;
+            }
+        } else if(totalWrong < totalCorrect) { // More correct than wrong
+            if(totalTime > 300) { // 5 minutes in seconds
+                return StudentStatus.THINKING;
+            } else {
+                return StudentStatus.HAPPY;
+            }
+        } else { // Same amount correct as wrong
+            return StudentStatus.THINKING;
+        }
     }
 
 
@@ -74,8 +111,70 @@ public class ControlCenter {
      * An enum used to represent the current status of the student.
      */
     enum StudentStatus {
-        WORRIED, HAPPY, THINKING, SORRY;
+        WORRIED, HAPPY, THINKING, SORRY
     }
 
+    /**
+     * Represents a stopwatch to be used to track how long a student has been doing a lesson.
+     *
+     * @author Matt Lillie
+     * @version 10/29/2017
+     */
+    class Stopwatch {
+
+        //Variables
+        final TimeUnit timeUnit;
+        boolean running = false;
+        long startTime = 0, endTime = 0;
+
+        /**
+         * Stopwatch constructor, takes in the time unit to be measured in.
+         * @param timeUnit The time unit that this stopwatch should be measured in.
+         */
+        Stopwatch(TimeUnit timeUnit) {
+            this.timeUnit = timeUnit;
+        }
+
+        /**
+         * Starts the stopwatch.
+         */
+        public void start() {
+            if(running) {
+                return;
+            }
+            this.running = true;
+            this.startTime = System.currentTimeMillis();
+        }
+
+        /**
+         * Stops the stopwatch.
+         */
+        public void stop() {
+            if(!running) {
+                return;
+            }
+            this.running = false;
+            this.endTime = System.currentTimeMillis();
+        }
+
+        /**
+         * Resets the stopwatch.
+         */
+        public void reset() {
+            this.running = false;
+            this.endTime = this.startTime = 0;
+        }
+
+        /**
+         * Gets the elapsed time of the stopwatch.
+         * @return The elapsed time of the stopwatch in the given TimeUnit.
+         */
+        public long elapsedTime() {
+            if(!running) {
+                return TimeUnit.MILLISECONDS.convert(endTime - startTime, timeUnit);
+            }
+            return TimeUnit.MILLISECONDS.convert(System.currentTimeMillis() - startTime, timeUnit);
+        }
+    }
 
 }
