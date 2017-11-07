625a1ba144e4d19d21ffcf27c1aadc2f07fe8082
argoerni <argoerni@asu.edu>
2017-11-06 19:51:49 -0700

    Update Companion.java

--- src/main/java/Companion.java
+++ src/main/java/Companion.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
+
 /**
+** implements Companion 
+* Rec Project 4
+*
 * @author Ashley Goernitz
 * @interface Companion:
 * @version 11/6/17
-* -implements Companion 
 */
 
 //Parent component to be used implemented in decorators
